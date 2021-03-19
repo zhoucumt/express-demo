d86f4d48c2ded7b07b08a57ab4d14af433663370
zhoucumt <553817478@qq.com>
2021-03-19 08:57:10 +0800

    express 简单实现

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 // 内部不是es6的写法  构造函数  异步处理 迭代 都是通过回调的方式
 
 // express 是一个函数 可以调用这个函数 创建一个应用
-const express = require('express');
+const express = require('./express');
 
 
 const app = express(); // 创建应用 
-
+    
 
 app.get('/',function (req,res) { // 没有ctx对象 主要靠的是原生的req和res 
     res.end('/') 
